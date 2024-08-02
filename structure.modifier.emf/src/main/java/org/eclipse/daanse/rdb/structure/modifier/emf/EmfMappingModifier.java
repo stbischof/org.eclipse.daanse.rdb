168242f50f8980da0699f4076a88527ce33d6278
Stefan Bischof <stbischof@bipolis.org>
2024-08-02 10:44:48 +0200

    bring emf model of rdb back into the repo of rdb api
    should now be possible for emf-generator that we use this in rolap from
    other repo. so we can bring it back from emf.model repo
    
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ structure.modifier.emf/src/main/java/org/eclipse/daanse/rdb/structure/modifier/emf/EmfMappingModifier.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *
+ */
+package org.eclipse.daanse.rdb.structure.modifier.emf;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.daanse.emf.model.rdbstructure.RelationalDatabaseFactory;
+import org.eclipse.daanse.rdb.modifier.AbstractMappingModifier;
+import org.eclipse.daanse.rdb.structure.api.model.Column;
+import org.eclipse.daanse.rdb.structure.api.model.DatabaseCatalog;
+import org.eclipse.daanse.rdb.structure.api.model.DatabaseSchema;
+import org.eclipse.daanse.rdb.structure.api.model.Link;
+import org.eclipse.daanse.rdb.structure.api.model.PhysicalTable;
+import org.eclipse.daanse.rdb.structure.api.model.SystemTable;
+import org.eclipse.daanse.rdb.structure.api.model.Table;
+import org.eclipse.daanse.rdb.structure.api.model.ViewTable;
+
+public class EmfMappingModifier extends AbstractMappingModifier {
+
+    protected EmfMappingModifier(DatabaseCatalog catalog) {
+        super(catalog);
+    }
+
+    @Override
+    protected Link createLink(Column primaryKey, Column foreignKey) {
+        org.eclipse.daanse.emf.model.rdbstructure.Link link = RelationalDatabaseFactory.eINSTANCE.createLink();
+        link.setPrimaryKey((org.eclipse.daanse.emf.model.rdbstructure.Column) primaryKey);
+        link.setForeignKey((org.eclipse.daanse.emf.model.rdbstructure.Column) foreignKey);
+        return link;
+    }
+
+    @Override
+    protected Column createColumn(
+        String name, Table table, String type, List<String> typeQualifiers,
+        String description
+    ) {
+        org.eclipse.daanse.emf.model.rdbstructure.Column column = RelationalDatabaseFactory.eINSTANCE.createColumn();
+        column.setName(name);
+        column.setTable((org.eclipse.daanse.emf.model.rdbstructure.Table) table);
+        column.setType(type);
+        column.getTypeQualifiers().addAll(typeQualifiers);
+        column.setDescription(description);
+        return column;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected ViewTable createViewTable(
+        String name, List<? extends Column> columns, DatabaseSchema schema,
+        String description
+    ) {
+        org.eclipse.daanse.emf.model.rdbstructure.ViewTable viewTable =
+            RelationalDatabaseFactory.eINSTANCE.createViewTable();
+        viewTable.setName(name);
+        viewTable.getColumns().addAll((Collection<? extends org.eclipse.daanse.emf.model.rdbstructure.Column>) columns);
+        viewTable.setSchema((org.eclipse.daanse.emf.model.rdbstructure.DatabaseSchema) schema);
+        viewTable.setDescription(description);
+        return viewTable;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected SystemTable createSystemTable(
+        String name, List<? extends Column> columns, DatabaseSchema schema,
+        String description
+    ) {
+        org.eclipse.daanse.emf.model.rdbstructure.SystemTable systemTable =
+            RelationalDatabaseFactory.eINSTANCE.createSystemTable();
+        systemTable.setName(name);
+        systemTable.getColumns().addAll((Collection<? extends org.eclipse.daanse.emf.model.rdbstructure.Column>) columns);
+        systemTable.setSchema((org.eclipse.daanse.emf.model.rdbstructure.DatabaseSchema) schema);
+        systemTable.setDescription(description);
+        return systemTable;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected PhysicalTable createPhysicalTable(
+        String name, List<? extends Column> columns, DatabaseSchema schema,
+        String description
+    ) {
+        org.eclipse.daanse.emf.model.rdbstructure.PhysicalTable physicalTable =
+            RelationalDatabaseFactory.eINSTANCE.createPhysicalTable();
+        physicalTable.setName(name);
+        physicalTable.getColumns().addAll((Collection<? extends org.eclipse.daanse.emf.model.rdbstructure.Column>) columns);
+        physicalTable.setSchema((org.eclipse.daanse.emf.model.rdbstructure.DatabaseSchema) schema);
+        physicalTable.setDescription(description);
+        return physicalTable;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected DatabaseSchema createDatabaseSchema(List<? extends Table> tables, String name, String id) {
+        org.eclipse.daanse.emf.model.rdbstructure.DatabaseSchema databaseSchema =
+            RelationalDatabaseFactory.eINSTANCE.createDatabaseSchema();
+        databaseSchema.getTables().addAll((Collection<? extends org.eclipse.daanse.emf.model.rdbstructure.Table>) tables);
+        databaseSchema.setName(name);
+        databaseSchema.setId(id);
+        return databaseSchema;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected DatabaseCatalog createCatalog(List<? extends DatabaseSchema> schemas, List<? extends Link> links) {
+        org.eclipse.daanse.emf.model.rdbstructure.DatabaseCatalog databaseCatalog = RelationalDatabaseFactory.eINSTANCE.createDatabaseCatalog();
+        databaseCatalog.getSchemas().addAll((Collection<? extends org.eclipse.daanse.emf.model.rdbstructure.DatabaseSchema>) schemas);
+        databaseCatalog.getLinks().addAll((Collection<? extends org.eclipse.daanse.emf.model.rdbstructure.Link>) links);
+        return databaseCatalog;
+    }
+
+}
